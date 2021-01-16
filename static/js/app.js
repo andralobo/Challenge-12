56a78a1f0e122516a5e4c5246a32d9030ecfe711
Andra Lobo <72619720+andralobo@users.noreply.github.com>
2021-01-15 22:18:44 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -80,7 +80,7 @@ function filterTable() {
   
 // 2. Attach an event to listen for changes to each filter
 
-d3.selectAll("input").on("change", updateFilters);
+d3.selectAll(".input-field").on("change", updateFilters);
 
 // Build the table when the page loads
 
